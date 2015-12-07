0c571f2ccb5ce80fcd3b4c06055200ad5e61c2ab
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2015-12-06 19:06:25 -0500

    Day 1: Part 2

---
+++ Day 1 - Not Quite Lisp/Part 2/Part 2.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+
+var counter = 0;
+
+fs.readFile("input.txt", "utf8", function(err, data) {
+	if(err) { throw err; }
+	for(i = 0; i < data.length; i++) {
+		if(data[i] == "(") { counter++; }
+		else { counter--; }
+		if(counter == -1) {
+			console.log("Position When Counter == -1: " + (i + 1));
+			break;
+		}
+	}
+});
