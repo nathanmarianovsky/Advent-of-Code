e5d9c53e882367152980683a7b6e87f7b6e9a431
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2015-12-07 22:58:06 -0500

    Day 5 - Part 2

---
+++ Day 5 - Doesn't He Have Intern-Elves For This/Part 2.js
@@ -0,0 +1,48 @@
+var fs = require("fs"),
+	total = 0;
+
+fs.readFile("input.txt", "utf8", function(err, data) {
+	if(err) { throw err; }
+	var collection = data.split("\n");
+	for(var i = 0; i < collection.length; i++) {
+		var pair = false,
+			repetition = false,
+			holder = [];
+		if(collection[i].length > 0) {
+			for(var j = 0; j < collection[i].length; j++) {
+				if((j + 2) != collection[i].length) {
+					var k = 0;
+					if((collection[i][j] == collection[i][j + 2])) { repetition = true; }
+					if((collection[i][j] != collection[i][j + 1]) || (collection[i][j + 1] != collection[i][j + 2]) || (collection[i][j] != collection[i][j + 2])) {
+						for(; k < holder.length; k++) {
+							if(collection[i][j] + collection[i][j + 1] == holder[k]) { pair = true; }
+						}
+						if(k == holder.length) {
+							holder.push(collection[i][j] + collection[i][j + 1]);
+						}
+					}
+					else {
+						for(; k < holder.length; k++) {
+							if(collection[i][j + 1] + collection[i][j + 2] == holder[k]) { pair = true; }
+						}
+						if(k == holder.length) {
+							holder.push(collection[i][j + 1] + collection[i][j + 2]);
+						}
+						j++;
+					}
+				}
+				else if((j + 1) != collection[i].length) {
+					var k = 0;
+					for(; k < holder.length; k++) {
+						if(collection[i][j] + collection[i][j + 1] == holder[k]) { pair = true; }
+					}
+					if(k == holder.length) {
+						holder.push(collection[i][j] + collection[i][j + 1]);
+					}
+				}
+			}
+		}
+		if((repetition == true) && (pair == true)) { total++; }
+	}
+	console.log("The total is: " + total);
+});
