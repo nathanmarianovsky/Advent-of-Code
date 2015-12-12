dcc8c82c2b21fc4e3e9490c7ad412f2c9ceb77ff
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2015-12-12 01:22:17 -0500

    Some Improvement on Day 12 - Part 1

--- Day 12 - JSAbacusFramework.io/Part 1.js
+++ Day 12 - JSAbacusFramework.io/Part 1.js
@@ -4,37 +4,30 @@ var fs = require("fs"),
 fs.readFile("input.txt", "utf8", function(err, data) {
 	if(err) { throw err; }
 	for(var i = 0; i < data.length;) {
-		var total = 0;
+		var total = 0,
+			new_str = "";
 		if(data[i] == "-") {
 			if(!isNaN(parseInt(data[i + 1]))) {
 				for(var p = i + 1; p < data.length; p++) {
-					if(isNaN(parseInt(data[p]))) { break; }
-					total++;
+					if(!isNaN(parseInt(data[p]))) { 
+						new_str += data[p];	
+						total++;
+					}
+					else { break; }
 				}
-				var new_str = "";
-				for(var k = i + 1; k < total + i + 1; k++) {
-					new_str += data[k];
-					console.log(new_str);
-				}
-				// if(new_str.length > 0) {
-					counter -= parseInt(new_str);
-				// }
+				counter -= parseInt(new_str);
 				total++;
 			}
 		}
 		else if(!isNaN(parseInt(data[i]))) {
 			for(var p = i; p < data.length; p++) {
-				if(isNaN(parseInt(data[p]))) { break; }
-				total++;
+				if(!isNaN(parseInt(data[p]))) { 
+					new_str += data[p];
+					total++;
+				}
+				else { break; }
 			}
-			var new_str = "";
-			for(var k = i; k < total + i; k++) {
-				new_str += data[k];
-				console.log(new_str);
-			}
-			// if(new_str.length > 0) {
-				counter += parseInt(new_str);
-			// }
+			counter += parseInt(new_str);
 		}
 		if(total > 0) { i += total; }
 		else { i++; }
