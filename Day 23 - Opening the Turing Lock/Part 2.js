edf4431f13c4869f8bcdbd881977bdf10f97fd09
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2015-12-23 05:12:45 -0500

    Day 23 - Part 2

---
+++ Day 23 - Opening the Turing Lock/Part 2.js
@@ -0,0 +1,43 @@
+var fs = require("fs");
+
+var type = {
+	"hlf": function(obj) { obj["value"] = Math.floor(obj["value"] / 2); },
+	"tpl": function(obj) { obj["value"] = obj["value"] * 3; },
+	"inc": function(obj) { obj["value"]++; }
+};
+
+fs.readFile("input.txt", "utf8", function(err, data) {
+	if(err) { throw err; }
+	var a = {
+		"name": "a",
+		"value": 1
+	};
+	var b = {
+		"name": "b",
+		"value": 0
+	};
+	var collection = data.split("\n"),
+		i = 0;
+	collection.splice(collection.length - 1, 1);
+	while(i < collection.length) {
+		var current = collection[i].split(" ");
+		if(current.length == 2) {
+			if((current[0] == "hlf") && (current[1] == "a")) { type["hlf"](a); i++; }
+			else if((current[0] == "hlf") && (current[1] == "b")) { type["hlf"](b); i++; }
+			else if((current[0] == "tpl") && (current[1] == "a")) { type["tpl"](a); i++; }
+			else if((current[0] == "tpl") && (current[1] == "b")) { type["tpl"](b); i++; }
+			else if((current[0] == "inc") && (current[1] == "a")) { type["inc"](a); i++; }
+			else if((current[0] == "inc") && (current[1] == "b")) { type["inc"](b); i++; }
+			else if(current[0] == "jmp") { i += parseInt(current[1]); }
+		}
+		else if(current.length == 3) {
+			current[1] = current[1].split(",")[0];
+			if((current[0] == "jie") && (current[1] == "a") && (a["value"] % 2 == 0)) { i += parseInt(current[2]); }
+			else if((current[0] == "jie") && (current[1] == "b") && (b["value"] % 2 == 0)) { i += parseInt(current[2]); }
+			else if((current[0] == "jio") && (current[1] == "a") && (a["value"] == 1)) { i += parseInt(current[2]); }
+			else if((current[0] == "jio") && (current[1] == "b") && (b["value"] == 1)) { i += parseInt(current[2]); }
+			else { i++; }
+		}
+	}
+	console.log("The value of 'b' is: " + b["value"]);
+});
