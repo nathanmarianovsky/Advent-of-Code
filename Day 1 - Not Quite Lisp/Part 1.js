40cf4a59fef1b2585e7d45293ddb08214d2b1b25
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2016-01-18 02:13:14 -0500

    Comments for Day 1

--- Day 1 - Not Quite Lisp/Part 1.js
+++ Day 1 - Not Quite Lisp/Part 1.js
@@ -1,6 +1,8 @@
+// Declare the necessary variables
 var fs = require("fs"),
 	counter = 0;
 
+// Read the file and parse. For each opening parenthesis add one to counter, otherwise subtract one from counter. The solution corresponds to the final value of counter.
 fs.readFile("input.txt", "utf8", function(err, data) {
 	if(err) { throw err; }
 	for(var i = 0; i < data.length; i++) {
--- Day 1 - Not Quite Lisp/Part 2.js
+++ Day 1 - Not Quite Lisp/Part 2.js
@@ -1,6 +1,8 @@
+// Declare the necessary variables
 var fs = require("fs"),
 	counter = 0;
 
+// Read the file and parse. For each opening parenthesis add one to counter, otherwise subtract one from counter. The solution corresponds to the first position that causes the counter to equate to -1.
 fs.readFile("input.txt", "utf8", function(err, data) {
 	if(err) { throw err; }
 	for(var i = 0; i < data.length; i++) {
