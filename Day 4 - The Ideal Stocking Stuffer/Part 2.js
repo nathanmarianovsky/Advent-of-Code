d661e7c31962aca892b44452fe145afa8ea6a138
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2016-01-18 02:38:49 -0500

    Comments for Day 4

--- Day 4 - The Ideal Stocking Stuffer/Part 2.js
+++ Day 4 - The Ideal Stocking Stuffer/Part 2.js
@@ -1,6 +1,8 @@
+// Declare the necessary variables
 var md5 = require("md5"),
 	input = "bgvyzdsv";
 
+// Checks whether or not a given string has an md5 hash with six leading zeros.
 var check = function(str) {
 	var result = md5(str),
 		counter = 0;
@@ -11,10 +13,12 @@ var check = function(str) {
 	else { return false; }
 };
 
+// For a given input the function determines the smallest natural number such that the md5 hash of the combined string (input + number) has six leading zeros.
 var find_result = function(str) {
 	var counter = 0;
 	while(!check(str + counter)) { counter++; }
 	return counter;
 };
 
+// The solution corresponds to the result returned from find_result(input).
 console.log("The value is: " + find_result(input));
