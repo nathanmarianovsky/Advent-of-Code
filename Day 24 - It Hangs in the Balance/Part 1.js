72fdd067c8e96d5a024c965dda0093813a6d4d3b
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2016-04-16 19:12:55 -0400

    Day 24 - Part 1
    Fixed the algorithm. The previous algorithm produced the correct answer
    by mere coincidence!

--- Day 24 - It Hangs in the Balance/Part 1.js
+++ Day 24 - It Hangs in the Balance/Part 1.js
@@ -1,64 +1,44 @@
 // Declare the necessary variable
-var fs = require("fs");
+var fs = require("fs"),
+    assortment = [];
 
-// Returns a set containing all possible subsets of the allowed numbers that form the target.
-var subsets = (numbers, target) => {
-    numbers = numbers.filter((value) => {
-        return value <= target;
-    });
-
-    numbers.sort((a, b) => {
-        return b - a;
-    });
-
-    var result = [];
-
-    while(numbers.length > 0) {
-        var i = 0,
-        	sum = 0,
-        	added_indices = [];
-        for(; i < numbers.length; i++) {
-            if(sum + numbers[i] <= target) {
-                sum += numbers[i];
-                added_indices.push(i);
-            }
-        }
-        var subset = [];
-        for(i = added_indices.length - 1; i >= 0; i--) {
-            subset.unshift(numbers[added_indices[i]]);
-            numbers.splice(added_indices[i], 1);
-        }
-        result.push(subset);
+// Produces all possible subsets that form the desired target.
+var subsets = (numbers, target, partial) => {
+    var sum = 0, 
+        current = 0, 
+        remaining = [];
+    partial = partial || [];
+    sum = partial.reduce((a, b) => { return a + b; }, 0);
+    if(sum === target) { assortment.push(partial); }
+    if(sum >= target) { return; }
+    for (var i = 0; i < numbers.length; i++) {
+        current = numbers[i];
+        remaining = numbers.slice(i + 1);
+        subsets(remaining, target, partial.concat([current]));
     }
-
-    for(var k = 0; k < result.length; k++) {
-    	var sum = result[k].reduce((left, right) => { return left + right; });
-    	if(sum !== target) { result.splice(k, 1); }
-    }
-
-    return result;
-}
+};
 
 // Read and parse. Once all of the allowed numbers and target are known, determine all possible subsets and compute the QE for the one with the least amount of numbers used.
 fs.readFile("input.txt", "utf8", (err, data) => {
 	if(err) { throw err; }
 	var holder = [],
-		collection = data.split("\n");
+        breakups = [],
+	   collection = data.split("\n");
 	collection.splice(collection.length - 1, 1);
 	collection = collection.map(elem => parseInt(elem));
-	var breakups = subsets(collection, collection.reduce((a,b) => { return a + b; }) / 3);
-	breakups.sort((left, right) => {
+    subsets(collection, collection.reduce((a,b) => { return a + b; }) / 3);
+	assortment.sort((left, right) => {
 		return left.length - right.length;
 	});
-	var ideal_limit = breakups[0].length,
+	var ideal_limit = assortment[0].length,
 		k = 1;
-	for(; k < breakups.length; k++) {
-		if(breakups[k].length !== ideal_limit) {
+	for(; k < assortment.length; k++) {
+		if(assortment[k].length !== ideal_limit) {
 			break;
 		}
 	}
 	for(var j = 0; j <= k; j++) {
-		var QE = breakups[j].reduce((left, right) => { return left * right; });
+		var QE = assortment[j].reduce((left, right) => { return left * right; }, 1);
 		holder.push(QE);
 	}
 	holder.sort((left, right) => {
