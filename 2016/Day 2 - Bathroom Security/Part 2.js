fa47308691fd6e7da84a2f889adf8ff5d8eb59f2
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2016-12-03 18:29:45 -0800

    Day 2 - Part 2
    Just a matter of redefining the evolution of states.

---
+++ 2016/Day 2 - Bathroom Security/Part 2.js
@@ -0,0 +1,96 @@
+// Declare the necessary variables
+var fs = require("fs"),
+	code = [];
+
+// Evolves any given state on the keypad according to the rules defined below.
+var evolution = (current, path) => {
+	var state = 0;
+	if(current == "1") {
+		if(path == "U" || path == "L" || path == "R") { state = "1"; }
+		else if(path == "D") { state = "3"; }
+	}
+	else if(current == "2") {
+		if(path == "U" || path == "L") { state = "2"; }
+		else if(path == "R") { state = "3"; }
+		else if(path == "D") { state = "6"; }
+	}
+	else if(current == "3") {
+		if(path == "U") { state = "1"; }
+		else if(path == "R") { state = "4"; }
+		else if(path == "D") { state = "7"; }
+		else if(path == "L") { state = "2"; }
+	}
+	else if(current == "4") {
+		if(path == "U" || path == "R") { state = "4"; }
+		else if(path == "D") { state = "8"; }
+		else if(path == "L") { state = "3"; }
+	}
+	else if(current == "5") {
+		if(path == "U" || path == "D" || path == "L") { state = "5"; }
+		else if(path == "R") { state = "6"; }
+	}
+	else if(current == "6") {
+		if(path == "U") { state = "2"; }
+		else if(path == "R") { state = "7"; }
+		else if(path == "D") { state = "A"; }
+		else if(path == "L") { state = "5"; }
+	}
+	else if(current == "7") {
+		if(path == "U") { state = "3"; }
+		else if(path == "R") { state = "8"; }
+		else if(path == "D") { state = "B"; }
+		else if(path == "L") { state = "6"; }
+	}
+	else if(current == "8") {
+		if(path == "U") { state = "4"; }
+		else if(path == "R") { state = "9"; }
+		else if(path == "D") { state = "C"; }
+		else if(path == "L") { state = "7"; }
+	}
+	else if(current == "9") {
+		if(path == "U" || path == "D" || path == "R") { state = "9"; }
+		else if(path == "L") { state = "8"; }
+	}
+	else if(current == "A") {
+		if(path == "L" || path == "D") { state = "A"; }
+		else if(path == "U") { state = "6"; }
+		else if(path == "R") { state = "B"; }
+	}
+	else if(current == "B") {
+		if(path == "U") { state = "7"; }
+		else if(path == "R") { state = "C"; }
+		else if(path == "D") { state = "D"; }
+		else if(path == "L") { state = "A"; }
+	}
+	else if(current == "C") {
+		if(path == "R" || path == "D") { state = "C"; }
+		else if(path == "U") { state = "8"; }
+		else if(path == "L") { state = "B"; }
+	}
+	else if(current == "D") {
+		if(path == "L" || path == "D" || path == "R") { state = "D"; }
+		else if(path == "U") { state = "B"; }
+	}
+	return state;
+};
+
+// Read the file and parse. For each each line of instructions evolve the start state accordingly and record the states at the end of each line.
+fs.readFile("input.txt", "utf8", function(err, data) {
+	if(err) { throw err; }
+	var container = data.split("\n"),
+		start = 5;
+	for(var i = 0; i < container.length; i++) {
+		if(container[i] != "") {
+			var instructions = container[i].split("");
+			for(var j = 0; j < instructions.length; j++) {
+				start = evolution(start, instructions[j]);
+			}
+			code.push(start);
+		}
+	}
+	var rep  = "";
+	for(var p = 0; p < code.length; p++) {
+		rep += code[p];
+	}
+	console.log("The bathroom code is " + rep + ".");
+});
--- 2016/Day 2 - Bathroom Security/README.md
+++ 2016/Day 2 - Bathroom Security/README.md
@@ -46,10 +46,11 @@ You finally arrive at the bathroom (it's a several minute walk from the lobby so
 
 You still start at "5" and stop when you're at an edge, but given the same instructions as above, the outcome is very different:
 
-You start at "5" and don't move at all (up and left are both edges), ending at 5.
-Continuing from "5", you move right twice and down three times (through "6", "7", "B", "D", "D"), ending at D.
-Then, from "D", you move five more times (through "D", "B", "C", "C", "B"), ending at B.
-Finally, after five more moves, you end at 3.
+- You start at "5" and don't move at all (up and left are both edges), ending at 5.
+- Continuing from "5", you move right twice and down three times (through "6", "7", "B", "D", "D"), ending at D.
+- Then, from "D", you move five more times (through "D", "B", "C", "C", "B"), ending at B.
+- Finally, after five more moves, you end at 3.
+
 So, given the actual keypad layout, the code would be 5DB3.
 
 Using the same instructions in your puzzle input, what is the correct bathroom code?
