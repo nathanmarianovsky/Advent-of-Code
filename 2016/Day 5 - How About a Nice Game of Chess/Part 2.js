cc94f6400b7830bdec7362641dc6169def780ccb
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2016-12-04 23:33:13 -0800

    Day 5 - Part 2

--- 2016/Day 5 - How About a Nice Game of Chess/Part 2.js
+++ 2016/Day 5 - How About a Nice Game of Chess/Part 2.js
@@ -1,6 +1,6 @@
 // Declare the necessary variables
 var md5 = require("md5"),
-	output = "",
+	output = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
 	input = "ffykfhsq";
 
 // Checks whether or not a given string has an md5 hash with five leading zeros.
@@ -14,8 +14,11 @@ var check = str => {
 	else { return false; }
 };
 
-// Extracts the 6th character of a md5 hash.
-var extract = str => { return md5(str)[5]; };
+// Extracts the 6th and 7th characters of a md5 hash.
+var extract = str => { 
+	var holder = md5(str);
+	return [parseInt(holder[5]), holder[6]]; 
+};
 
 // For a given input the function determines the smallest natural number such that the md5 hash of the combined string (input + number) has five leading zeros.
 var find_result = (str, start) => {
@@ -25,12 +28,20 @@ var find_result = (str, start) => {
 };
 
 // Loops through until the whole password is generated.
-var count = 0,
-	extraction = "";
-for(var k = 0; k < 8; k++) {
+var count = 0;
+while(output.some(elem => elem == undefined)) {
 	count = find_result(input, count + 1);
-	extraction = extract(input + count.toString());
-	output += extraction;
+	var extraction = extract(input + count.toString());
+	if(extraction[0] >= 0 && extraction[0] <= 7 && extraction[0] !== NaN) {
+		if(output[extraction[0]] == undefined) {
+			output[extraction[0]] = extraction[1];
+		}
+	}
 }
 
-console.log("The password for the security door is " + output + ".");
+var password = "";
+for(var j = 0; j < output.length; j++) {
+	password += output[j];
+}
+
+console.log("The password for the security door is " + password + ".");
