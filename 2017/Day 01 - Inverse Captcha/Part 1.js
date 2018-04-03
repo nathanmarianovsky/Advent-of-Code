da4317d0469fa2a675e7a7919e093e210306f425
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2018-04-03 15:20:22 -0700

    Day 01 - Part 1

---
+++ 2017/Day 01 - Inverse Captcha/Part 1.js
@@ -0,0 +1,21 @@
+// Declare the necessary variables
+var fs = require("fs");
+
+// Read the file and parse. At each step compare the digits and add if they match.
+fs.readFile("input.txt", "utf8", function(err, data) {
+	if(err) { throw err; }
+	var	container = data.split(""),
+		sum = 0;
+	container.splice(container.length - 1,1);
+	for(var j = 0; j < container.length; j++) {
+		var current = parseInt(container[j]);
+		if(j != container.length - 1) {
+			var next = parseInt(container[j+1]);
+			if(current == next) { sum += current; }
+		}
+		else {
+			if(current == parseInt(container[0])) { sum += current; }
+		}
+	}
+	console.log("The solution to the captcha is " + sum + ".");
+});
