57c2a5ea3547631a050ed73aeaf74edbeb9a41db
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2018-06-24 02:29:47 -0700

    Day 09 - Part 1

---
+++ 2017/Day 09 - Stream Processing/Part 1.js
@@ -0,0 +1,21 @@
+// Declare the necessary variables
+var fs = require("fs");
+
+// Read the file and parse. Iterate through the string and keep count along the way.
+fs.readFile("input.txt", "utf8", function(err, data) {
+	if(err) { throw err; }
+	var container = data.split("\n"),
+		str = container[0];
+	 	condition = false,
+	 	count = 0,
+	 	level = 1;
+	for(var i = 0; i < str.length; i++) {
+		if (str[i] == "!") { i++; }
+		else if (condition && str[i] != ">") {}
+		else if(str[i] == "<") { condition = true; }
+		else if(str[i] == ">") { condition = false; }
+		else if(str[i] == "{") { count += level++; }
+		else if(str[i] == "}") { level--; }
+	}
+	console.log("The total score for all of the groups in the given input is " + count + ".");
+});
