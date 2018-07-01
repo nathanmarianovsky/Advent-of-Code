4126e130b7bad73c64a9905d3cb6ba025c4b2212
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2018-06-30 23:15:41 -0700

    Day 11 - Part 2

---
+++ 2017/Day 11 - Hex Ed/Part 2.js
@@ -0,0 +1,57 @@
+// Declare the necessary variables
+var fs = require("fs");
+
+// Given the next step and the list of current steps already taken, this returns the new reduced list of steps.
+var check = (value, steps) => {
+	if(value == "e" && steps.indexOf("w") != -1) { steps.splice(steps.indexOf("w"), 1); }
+	else if(value == "w" && steps.indexOf("e") != -1) { steps.splice(steps.indexOf("e"), 1); }
+	else if(value == "n" && steps.indexOf("s") != -1) { steps.splice(steps.indexOf("s"), 1); }
+	else if(value == "s" && steps.indexOf("n") != -1) { steps.splice(steps.indexOf("n"), 1); }
+	else if(value == "ne" && steps.indexOf("sw") != -1) { steps.splice(steps.indexOf("sw"), 1); }
+	else if(value == "sw" && steps.indexOf("ne") != -1) { steps.splice(steps.indexOf("ne"), 1); }
+	else if(value == "nw" && steps.indexOf("se") != -1) { steps.splice(steps.indexOf("se"), 1); }
+	else if(value == "se" && steps.indexOf("nw") != -1) { steps.splice(steps.indexOf("nw"), 1); }
+	else if(value == "e" && steps.indexOf("nw") != -1) { steps[steps.indexOf("nw")] = "ne"; }
+	else if(value == "nw" && steps.indexOf("e") != -1) { steps[steps.indexOf("e")] = "ne"; }
+	else if(value == "e" && steps.indexOf("sw") != -1) { steps[steps.indexOf("sw")] = "se"; }
+	else if(value == "sw" && steps.indexOf("e") != -1) { steps[steps.indexOf("e")] = "se"; }
+	else if(value == "n" && steps.indexOf("sw") != -1) { steps[steps.indexOf("sw")] = "nw"; }
+	else if(value == "sw" && steps.indexOf("n") != -1) { steps[steps.indexOf("n")] = "nw"; }
+	else if(value == "n" && steps.indexOf("se") != -1) { steps[steps.indexOf("se")] = "ne"; }
+	else if(value == "se" && steps.indexOf("n") != -1) { steps[steps.indexOf("n")] = "ne"; }
+	else if(value == "w" && steps.indexOf("se") != -1) { steps[steps.indexOf("se")] = "sw"; }
+	else if(value == "se" && steps.indexOf("w") != -1) { steps[steps.indexOf("w")] = "sw"; }
+	else if(value == "w" && steps.indexOf("ne") != -1) { steps[steps.indexOf("ne")] = "nw"; }
+	else if(value == "ne" && steps.indexOf("w") != -1) { steps[steps.indexOf("w")] = "nw"; }
+	else if(value == "s" && steps.indexOf("ne") != -1) { steps[steps.indexOf("ne")] = "se"; }
+	else if(value == "ne" && steps.indexOf("s") != -1) { steps[steps.indexOf("s")] = "se"; }
+	else if(value == "s" && steps.indexOf("nw") != -1) { steps[steps.indexOf("nw")] = "sw"; }
+	else if(value == "nw" && steps.indexOf("s") != -1) { steps[steps.indexOf("s")] = "sw"; }
+	else if(value == "se" && steps.indexOf("sw") != -1) { steps[steps.indexOf("sw")] = "s"; }
+	else if(value == "sw" && steps.indexOf("se") != -1) { steps[steps.indexOf("se")] = "s"; }
+	else if(value == "ne" && steps.indexOf("se") != -1) { steps[steps.indexOf("se")] = "e"; }
+	else if(value == "se" && steps.indexOf("ne") != -1) { steps[steps.indexOf("ne")] = "e"; }
+	else if(value == "ne" && steps.indexOf("nw") != -1) { steps[steps.indexOf("nw")] = "n"; }
+	else if(value == "nw" && steps.indexOf("ne") != -1) { steps[steps.indexOf("ne")] = "n"; }
+	else if(value == "sw" && steps.indexOf("nw") != -1) { steps[steps.indexOf("nw")] = "w"; }
+	else if(value == "nw" && steps.indexOf("sw") != -1) { steps[steps.indexOf("sw")] = "w"; }
+	else { steps.push(value); }
+	return steps;
+};
+
+// Read the file and parse. Iterate through the given steps and reduce the number of steps required while walking. Now keep a tab on the lengths per step and return the biggest value obtained. 
+fs.readFile("input.txt", "utf8", function(err, data) {
+	if(err) { throw err; }
+	var input = data.split("\n")[0].split(","),
+		steps = [],
+		lengths = [];
+	steps[0] = input[0];
+	for(var i = 1; i < input.length; i++) { 
+		steps = check(input[i], steps); 
+		lengths.push(steps.length);
+	}
+	lengths.sort((a,b) => {
+		return b - a;
+	});
+	console.log("The furthest the child process traveled required " + lengths[0] + " steps.");
+});
