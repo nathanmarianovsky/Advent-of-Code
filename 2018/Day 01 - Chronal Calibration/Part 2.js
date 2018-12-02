4bb01df885fb7e9b856d51d634013e715ad30137
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2018-12-01 16:35:35 -0800

    Day 01 - Part 2

---
+++ 2018/Day 01 - Chronal Calibration/Part 2.js
@@ -0,0 +1,22 @@
+// Declare the necessary variables
+var fs = require("fs");
+
+// Read the file and parse. At each step update the frequency, keep track of the previous values,
+// and return the first value that is reached twice.
+fs.readFile("input.txt", "utf8", (err, data) => {
+	if(err) { throw err; }
+	var	container = data.split("\n").map(elem => parseInt(elem)),
+		hits = [],
+		pos = 0,
+		frequency = 0;
+	container.pop();
+	while(true) {
+		frequency += container[pos];
+		if(hits.some(elem => elem == frequency)) { break; }
+		else {
+			hits.push(frequency);
+			pos + 1 == container.length ? pos = 0 : pos++;
+		}
+	}
+	console.log("The first frequency the device reaches twice is " + frequency + ".");
+});
