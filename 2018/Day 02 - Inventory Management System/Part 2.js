a1f84f4c422c28352539eaa91ac0c578526695e8
Nathan Marianovsky <nathanmarianovsky@manualmath.com>
2020-09-07 12:21:58 -0700

    Day 02 - Part 2

---
+++ 2018/Day 02 - Inventory Management System/Part 2.js
@@ -0,0 +1,32 @@
+// Declare the necessary variables
+var fs = require("fs");
+
+// Read the file and parse. For each string determine if there are repeating characters, count them, and compute the checksum.
+fs.readFile("input.txt", "utf8", (err, data) => {
+	if(err) { throw err; }
+	var	container = data.split("\n").map(elem => elem.split("")),
+		counter = 0,
+		place = 0,
+		index = 0;
+	container.pop();
+	for(var i = 0; i < container.length; i++) {
+		var j = i + 1;
+		for(; j < container.length; j++) {
+			counter = 0;
+			index = 0;
+			for(var k = 0; k < container[i].length; k++) {
+				if(container[i][k] != container[j][k]) {
+					counter++;
+					index = k;
+				}
+			}
+			if(counter == 1) {
+				place = i;
+				break;
+			}
+		}
+		if(j < container.length) { break; }
+	}
+	container[place].splice(index, 1);
+	console.log("The resulting common letters between the two correct IDs are '" + container[place].join("") + "'.");
+});
